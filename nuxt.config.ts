5fa6b6ddc6321ccb36d41013e84c0ed861cc7755
HIsheR <shawn1440982358@163.com>
2024-08-23 10:21:27 +0800

    style: 优化一些tailwindcss的基础样式

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,50 +1,70 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-07-10',
+  compatibilityDate: "2024-07-10",
   devtools: { enabled: true },
   modules: ["@element-plus/nuxt", "@nuxtjs/tailwindcss"],
   app: {
     head: {
-      title: '标签栏网站名称',
-      charset: 'utf-8',
-      bodyAttrs: { style: 'height: 100%; margin:0; padding: 0;' },
-      link: [{ rel: 'icon', href: '/favicon.ico', type: "images/x-icon" }],
+      title: "Nuxt3 + Tailwindcss快速开发模板",
+      charset: "utf-8",
+      bodyAttrs: { style: "height: 100%; margin:0; padding: 0;" },
+      link: [{ rel: "icon", href: "/favicon.ico", type: "images/x-icon" }],
       meta: [
         {
-          hid: "description", name: 'description', content: 'description'
+          hid: "description",
+          name: "description",
+          content: "your description",
         },
-        { name: 'keywords', content: 'keywords' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'applicable-device', content: 'pc' },
+        { name: "keywords", content: "your keywords" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: "applicable-device", content: "pc" },
         {
           name: "HIsheR", // 网站作者
           content: "HIsheR",
         },
         { "http-equiv": "X-UA-Compatible", content: "IE=edge,chrome=1" }, // 浏览器优先打开谷歌和edge
         { "http-equiv": "Cache-Control", content: "no-transform" },
-
       ],
     },
   },
-  // https://nuxt.com.cn/docs/api/composables/use-runtime-config
+  // 参考文档 https://nuxt.com.cn/docs/api/composables/use-runtime-config
   runtimeConfig: {
     // 对客户端暴露的内容
     public: {
       proxyUrl: process.env.NUXT_PROXY_URL,
-    }
+    },
   },
   // 指定路由配置
   routeRules: {
     // 请求转发
-    '/prod-api/**': {
+    "/prod-api/**": {
       proxy: `${process.env.NUXT_BASE_URL}/**`,
     },
   },
-
+  // 参考文档 https://tailwindcss.nuxtjs.org/getting-started/installation#options
+  tailwindcss: {
+    // Options
+    // https://tailwindcss.nuxtjs.org/getting-started/configuration
+    cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }], // 指定tailwind.css路径
+    configPath: "tailwind.config",
+    // exposeConfig: {
+    //   level: 2,
+    // },
+    config: {},
+    viewer: true,
+  },
+  // 导入第三方样式库
+  css: [
+    "~/assets/css/flex.css",
+  ],
   postcss: {
     plugins: {
-      "autoprefixer": {},
-      "cssnano": {}
-    }
+      autoprefixer: {},
+      cssnano: {},
+    },
   },
-})
+  image: {
+    quality: 80,
+    format: ["webp"],
+  },
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,43 +1,82 @@
-import type { Config } from 'tailwindcss'
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './components/**/*.vue',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-  ],
+import type { Config } from "tailwindcss"
+// 自定义样式 https://tailwindcss.com/docs/configuration
+export default <Partial<Config>>{
+  content: ["./components/**/*.vue", "./layouts/**/*.vue", "./pages/**/*.vue"],
   theme: {
-    // 参考文档 https://www.tailwindcss.cn/docs/installation
+    // 放在extend下面是拓展样式
     extend: {
+      screens: {
+        "3xl": "2000px",
+      },
       colors: {
-        'gray1': '#000000',
-        'gray2': '#333333',
-        'gray3': '#666666',
-        'gray4': '#999999',
-        'gray5': '#D9D9D9',
-        'gray6': '#EAEAEA',
-        'gray7': '#FAFAFA',
-        'gray8': '#FFFFFF',
+        gray1: "#000000",
+        gray2: "#333333",
+        gray3: "#666666",
+        gray4: "#999999",
+        gray5: "#D9D9D9",
+        "gray5-3": "#EAEAEA",
+        gray6: "#EAEAEA",
+        gray7: "#FAFAFA",
+        gray8: "#FFFFFF",
+        gray9: "#A8ABB2",
 
-        'brand1': '#007AFF',
-        'brand2': '#F2F7FF',
+        brand1: "#007AFF",
+        brand2: "#F2F7FF",
 
-        'alert': '#D62020',
-        'green': '#18C37C',
-        'purple': '#9000FF',
-        'orange': '#E89E42',
+        alert: "#D62020",
+        green: "#18C37C",
+        purple: "#9000FF",
+        orange: "#E89E42",
+
+        "tips-color": "#999999",
+        "border-color": "#D9D9D9",
+      },
+      padding: {
+        first: "40px",
+        two: "20px",
+        base: "10px",
+      },
+      margin: {
+        first: "40px",
+        two: "20px",
+        base: "10px",
+      },
+      gap: {
+        first: "40px",
+        two: "20px",
+        base: "10px",
+      },
+      container: {
+        center: true,
+      },
+      minWidth: {
+        1024: "1024px",
+      },
+      maxWidth: {
+        1024: "1024px",
+        1280: "1280px",
+      },
+      fontSize: {
+        large: ["24px", "30px"],
+        first: ["20px", "26px"],
+        two: ["16px", "22px"],
+        base: ["14px", "20px"],
+        tips: ["12px", "18px"],
+      },
+      fontFamily: {
+        base: [
+          "PingFang SC",
+          "Microsoft YaHei",
+          "system-ui",
+          "Helvetica Neue",
+          "Microsoft Yahei",
+          "Hiragino Sans GB",
+          "Heiti SC",
+          "WenQuanYi Micro Hei",
+        ],
       },
     },
-    // 参考文档 https://www.tailwindcss.cn/docs/font-family#using-custom-values
-    fontFamily: {
-      'sans': ['PingFang SC', 'Microsoft YaHei']
-    },
-    // 参考文档 https://tailwindcss.com/docs/font-size#customizing-your-theme
-    fontSize: {
-      'first': ['20px', '26px'],
-      'two': ['16px', '22px'],
-      'base': ['14px', '20px'],
-      'tips': ['12px', '18px']
-    }
+    // 不在extend下面的话是覆盖默认样式
   },
-} satisfies Config
+}
