5deb5741f599831e0ce44d097db963acb46c95d4
ClungTsang <shawn1440982358@163.com>
2024-09-23 20:31:37 +0800

    feat: 新增常用的图片等待组件和空图片组件

---
+++ components/EmptyImg/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-empty :class="className" :image-size="size">
+    <template #image>
+      <slot name="image"></slot>
+    </template>
+    <template #description>
+      <div class="text-tips text-tips-color font-normal text-center text-nowrap">
+        {{ description }}
+      </div>
+    </template>
+  </el-empty>
+</template>
+<script lang="ts" setup>
+defineProps({
+  size: {
+    type: Number,
+    default: 150,
+  },
+  className: {
+    type: String,
+    default: "aspect-auto",
+  },
+  description: {
+    type: String,
+    default: "暂无数据",
+  },
+});
+</script>
---
+++ components/LoadingImg/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-skeleton animated :loading="loading" class="w-full" :class="className">
+    <template #template>
+      <el-skeleton-item :variant="variant" :class="className" style="width: 100%; height: 100%" />
+    </template>
+    <slot></slot>
+  </el-skeleton>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  loading: {
+    type: Boolean,
+    default: true,
+  },
+  className: {
+    type: String,
+    default: "aspect-video",
+  },
+  variant: {
+    type: String,
+    default: "image",
+  },
+});
+</script>
