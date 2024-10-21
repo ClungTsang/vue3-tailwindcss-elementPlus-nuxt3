398a2a031a7a5fb522609baf904e807454745443
HIsheR <shawn1440982358@163.com>
2024-10-21 16:41:52 +0800

    feat: 去除弹框

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="text-center">
-    <div v-html="result?.content"> </div>
-    <el-button type="primary" size="default" @click="changeTopic">切换文章</el-button>
+    <div v-html="result?.content"></div>
+    <el-button
+      type="primary"
+      size="default"
+      @click="changeTopic"
+      >切换文章</el-button
+    >
   </div>
 </template>
 <script setup lang="ts">
-import { ElMessage } from 'element-plus'
-const { interaction } = useApi()
-const result = ref()
+const { interaction } = useApi();
+const result = ref();
 const initTopic = async () => {
-  const { data } = await interaction.getTopics('6262718079f90d94a6a0d2f5')
-  ElMessage.success('初始化数据成功')
-  result.value = data
-}
+  const { data } = await interaction.getTopics("5433d5e4e737cbe96dcef312");
+  result.value = data;
+};
 // 初始化数据
-initTopic()
+initTopic();
 
 const changeTopic = async () => {
-  const { data } = await interaction.getTopics('634fa473d3061fe5e4e88d35')
-  ElMessage.success('获取数据成功')
-  result.value = data
-}
+  const { data } = await interaction.getTopics("634fa473d3061fe5e4e88d35");
+  result.value = data;
+};
 </script>
 <style scoped lang="scss"></style>
